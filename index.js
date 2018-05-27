eb66691ddbf21d1e0de85f6535833d93db014f73
anhgames78 <anhnt78@gmail.com>
2018-05-27 21:19:01 +0700

    express

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Hello to Nodejs");
-console.log("This is new");
+var express = require("express");
+var app = express();
+app.listen(3000);
+
+app.get("/hello", function (req, res){
+res.send("<font color=red>Hello Nodejs</font>");
+});
