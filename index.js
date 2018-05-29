39ce069d7c4225fc49cd5c3b73b16dd4adaca996
anhgames78 <anhnt78@gmail.com>
2018-05-30 02:21:45 +0700

    hello/abc

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 app.listen(3000);
 
+app.get("/hello/:id", function (req, res){
+var i = req.params.id;
+res.send("<font color=blue>Hello Nodejs. </font>"+"<font color=red>Server nhan id="+i+"</font>");
+});
 app.get("/hello", function (req, res){
-res.send("<font color=blue>Hello Nodejs</font>");
+res.send("<font color=red>Hello Nodejs.</font>");
 });
