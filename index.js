82fb4b2a7e9cbceca1030cd0e627710ef138020b
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2018-10-01 08:22:58 +0700

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.listen(3000);
+app.listen(8080);
 
 app.get("/hello", function (req, res){
 res.send("<font color=red>Hello Nodejs</font>");
