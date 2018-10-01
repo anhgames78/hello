2846243209b645a8f8461660e9b3e062eaf15c4a
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2018-10-01 08:28:54 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require("express");
 var app = express();
 app.listen(8080);
 
-app.get("/hello", function (req, res){
+app.get("/", function (req, res){
 res.send("<font color=red>Hello Nodejs</font>");
 });
